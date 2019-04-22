7bb39b8675fbe522b54a9e786229415008aa0626
Roberta <robertaf898@gmail.com>
2019-04-22 16:05:22 +0100

    Gets key from google to load map in the browser

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+ <div>
+   <google-map name="example"></google-map>
+ </div>
+</template>
+
+<script>
+ import googleMap from './googleMap.vue'
+
+  export default {
+  
+    name: 'App',
+
+    methods: {
+      
+    },
+
+    components: {
+      "google-map": googleMap
+    }
+
+  
+  }
+
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
