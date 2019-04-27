e016542f882a2c59381ac90f77c97040c9fe5523
Roberta <robertaf898@gmail.com>
2019-04-27 15:13:19 +0100

    Adds marker to map when user clicks on it and creates a bounding box around the click and also draws markers on the points that constitute the box.

--- src/googleMap.vue
+++ src/googleMap.vue
@@ -22,21 +22,46 @@
             const map = new google.maps.Map(element, options);
 
             map.addListener('click', function(event) {
-                const lat = event.qa.y;
-                const long = event.qa.x;
-                const coordinates = getCoordinates(lat, long);
-                console.log("THESE ARE MY COORDINATES:", coordinates)
+                console.log("evventtt", event)
+                const lat = event.latLng.lat();
+                const long = event.latLng.lng();
+                const coordinates = {"latitude": lat, "longitude": long};
+                console.log("THESE ARE MY COORDINATES:", coordinates);
+                const a = 0.025;
+                const b = 0.025;
+                const latMax = lat + a;
+                const latMin = lat - a;
+                const longMax = long + b;
+                const longMin = long - b;
+                const boundingCoordinates = {"latitudeMax": latMax, "latitudeMin": latMin, "longitudeMax": longMax, "longitudeMin": longMax} 
+                console.log("these are the LatMax, LatMin, LongMax, LongMin",boundingCoordinates);
+                const endMarker = new google.maps.Marker({
+                    position: new google.maps.LatLng(lat,long),
+                    map: map,
+                    draggable: false,
+                });
+                const p1 = new google.maps.Marker({
+                    position: new google.maps.LatLng(latMax,long),
+                    map: map,
+                    draggable: false,
+                });
+                   const p2 = new google.maps.Marker({
+                    position: new google.maps.LatLng(latMin,long),
+                    map: map,
+                    draggable: false,
+                });
+                   const p3 = new google.maps.Marker({
+                    position: new google.maps.LatLng(lat,longMax),
+                    map: map,
+                    draggable: false,
+                });
+                   const p4 = new google.maps.Marker({
+                    position: new google.maps.LatLng(lat,longMin),
+                    map: map,
+                    draggable: false,
+                });
             })
-            
-            function getCoordinates(latY, longX) {
-                const coordinates = {"latitude": latY, "longitude": longX}
-                return coordinates
-            }
 
-            function createBoundingBox(latMin, latMax, longMin, longMax) {
-                const boundingCoordinates = {"latitudeMin": latMin, "latitudeMax": latMax, "longitudeMin": longMin, "longitudeMax": longMax} 
-                return boundingCoordinates
-            }
         }
     }
 </script>
